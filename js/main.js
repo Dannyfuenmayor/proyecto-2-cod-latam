0355a83b86bca1d307a8c244295db96eed20bc8e
maiEidos <mai@eidosglobal.org>
2023-06-30 23:53:06 -0400

    Se corrige código de JS y se hace funcionar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-menuItems.forEach(function(item) {
-    item.addEventlistener ('click', function(e) {
+const menuItems = document.querySelectorAll( '.item-menu1');
+
+menuItems.forEach(function (item) {
+    item.addEventListener('click', function(e) {
         const currentItem = document.querySelector('.active');
         currentItem.classList.remove('active');
         e.target.classList.add('active');
     });
-    
 });
